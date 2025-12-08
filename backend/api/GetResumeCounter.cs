0706175bee69a1cbb8ade035371605807dd9b2d2
DrQ-14 <skolwaite@gmail.com>
2025-12-07 20:51:31 -0500

    Namespaces and Syntax fixed, ready for cosmos DB Binding. No job functions are yet implemented.

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,23 +1,35 @@
-using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Company.Function;
 
 public class GetResumeCounter
 {
-    private readonly ILogger<GetResumeCounter> _logger;
 
-    public GetResumeCounter(ILogger<GetResumeCounter> logger)
+    [FunctionName("GetResumeCounter")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+    
+        ILogger log)
     {
-        _logger = logger;
-    }
+        log.LogInformation("C# HTTP trigger function processed a request.");
+        
+        string name = req.Query["name"];
+        
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        name = name ?? data?.name;
 
-    [Function("GetResumeCounter")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-    {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        string responseMessage = string.IsNullOrEmpty(name)
+            ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            : $"Hello, {name}. This HTTP triggered function executed successfully.";
+        
+        return new OkObjectResult(responseMessage);
     }
 }
