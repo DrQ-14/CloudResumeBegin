9aa0aab065b5f6ae89215fecc5c1f7b742ec94ca
DrQ-14 <skolwaite@gmail.com>
2025-12-25 01:55:07 -0500

    Mostly done with initial CosmosDB connection, it does not register the connection endpoint for some reason. Will look into why with a fresh pair of eyes later.

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,7 +1,14 @@
+using System.Configuration;
+using System.Reflection.Metadata;
+using Grpc.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net;
 
 namespace Company.Function;
 
@@ -9,10 +16,26 @@ public class GetResumeCounter
 {
     private readonly ILogger<GetResumeCounter> _logger; 
 
-    public static Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get","post", Route = null)] HttpRequest req,
-        ILogger log)
-    {
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get","post", Route = null)] HttpRequestData req,
+
+        //CosmosDB input binding 
+        [CosmosDBTrigger(
+            databaseName: "Counter",
+            containerName: "AzureResume",
+            Connection = "CosmosDBConnection")]
+        IEnumerable<Counter> inputItems)
         
+        //CosmosDB output binding
+        /*[CosmosDBTrigger(
+            databaseName: "Counter", 
+            containerName: "AzureResume",
+            Connection = "cloud-resume-for-me")]
+            IAsyncCollector<Counter> outputItem
+        )*/
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(inputItems);
+        return response;
     }
 }
